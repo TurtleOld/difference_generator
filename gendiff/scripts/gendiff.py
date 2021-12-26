4608eb92fa148fc1aa8041c8d05d8fcc05814fca
Alexander <dev@pavlovteam.ru>
2021-12-26 18:36:12 +0300

    first task in done

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Generate diff')
+parser.add_argument('first_file')
+parser.add_argument('second_file')
+
+args = parser.parse_args()
+print(args.first_file(args.second_file))
