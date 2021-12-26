ed2f5fa271ac95a46cda78be9c5cd389ddd7700a
Alexander <dev@pavlovteam.ru>
2021-12-26 20:32:52 +0300

    change

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import argparse
+import json
 
 parser = argparse.ArgumentParser(prog='gendiff', description='Generate diff')
 parser.add_argument('first_file')
@@ -9,3 +10,8 @@ parser.add_argument('-f', '--format', type=str, help='set format of output')
 
 args = parser.parse_args()
 print(args.first_file(args.second_file))
+
+
+def generate_diff(filepath1, filepath2):
+    file1 = json.load(filepath1)
+    file2 = json.load(filepath2)
