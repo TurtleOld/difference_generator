489e349b0d4657b0cc350ac56ed828a1b8d4380f
Alexander <dev@pavlovteam.ru>
2021-12-26 20:11:56 +0300

    second task in done

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,9 +2,10 @@
 
 import argparse
 
-parser = argparse.ArgumentParser(description='Generate diff')
+parser = argparse.ArgumentParser(prog='gendiff', description='Generate diff')
 parser.add_argument('first_file')
 parser.add_argument('second_file')
+parser.add_argument('-f', '--format', type=str, help='set format of output')
 
 args = parser.parse_args()
 print(args.first_file(args.second_file))
