f5161f9d662133228c5e6faa58fde03c12d83db5
TurtleOld <shurpo.alexander@gmail.com>
2022-01-04 23:25:03 +0300

    Added file with text for tests

--- gendiff/tests/test_generate_diff.py
+++ gendiff/tests/test_generate_diff.py
@@ -1,4 +1,6 @@
 from gendiff import generate_diff
+from fixtures.result import result
+
 
 file1 = 'file1.json'
 file2 = 'file2.json'
@@ -6,8 +8,4 @@ file2 = 'file2.json'
 
 def test_generate_diff():
     assert type(generate_diff(file1, file2)) == str
-    assert generate_diff(file1, file2) == '{\n - follow: False\n   ' \
-                                          'host: hexlet.io\n' \
-                                          ' - proxy: 123.234.53.22\n' \
-                                          ' - timeout: 50\n' \
-                                          ' + timeout: 20\n + verbose: True\n}'
+    assert generate_diff(file1, file2) == result
