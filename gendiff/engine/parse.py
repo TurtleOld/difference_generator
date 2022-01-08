13fbdbe6bb1507e05371fa2ec4b17f18af56b98a
TurtleOld <shurpo.alexander@gmail.com>
2022-01-08 22:30:53 +0300

    add arguments of version program

--- gendiff/engine/parse.py
+++ gendiff/engine/parse.py
@@ -2,16 +2,21 @@ import argparse
 from gendiff import generate_diff
 
 
-DESCRIPTION = 'Generate diff'
+DESCRIPTION = 'Difference generator'
 FORMAT_FLAG_1 = '-f'
 FORMAT_FLAG_2 = '--format'
-FORMAT_HELP = 'set format of output'
+FORMAT_HELP = "set format of output (default 'stylish')"
+VERSION_STRING_1 = '-v'
+VERSION_STRING_2 = '--version'
+VERSION_NUMBER = '{0} 1.0'.format(DESCRIPTION)
 
 
 def parsing():
     parser = argparse.ArgumentParser(description=DESCRIPTION)
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
+    parser.add_argument(VERSION_STRING_1, VERSION_STRING_2, action='version',
+                        version='{0}'.format(VERSION_NUMBER))
     parser.add_argument(FORMAT_FLAG_1, FORMAT_FLAG_2,
                         help=FORMAT_HELP,
                         default='stylish')
