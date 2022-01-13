5e16506212eaae8366eb5c033337699d59fba031
TurtleOld <shurpo.alexander@gmail.com>
2022-01-13 21:45:19 +0300

    refactor

--- gendiff/engine/finding_difference.py
+++ gendiff/engine/finding_difference.py
@@ -1,4 +1,5 @@
 """ Creating representation difference. """
+
 VALUE_DELETED = 'deleted'
 VALUE_ADDED = 'added'
 VALUE_CHANGED = 'changed'
@@ -6,22 +7,7 @@ VALUE_UNCHANGED = 'unchanged'
 VALUE_NESTED = 'nested'
 
 
-def create_difference_segment(vertex_type, key, value1, value2=None,
-                              nested=None):
-    """
-    Функция-аккумулятор ключей.
-    """
-    collected_data = {
-        'type': vertex_type,
-        'key': key,
-        'value1': value1,
-        'value2': value2,
-        'nested': nested
-    }
-    return collected_data
-
-
-def create_tree_vertex(dictionary_1: dict, dictionary_2: dict):
+def create_tree_vertex(dictionary_1: dict, dictionary_2: dict) -> list:
     """
     Функция создания разницы между данными двух файлов.
     :param dictionary_1: Словарь.
@@ -35,34 +21,36 @@ def create_tree_vertex(dictionary_1: dict, dictionary_2: dict):
 
     for dict_key in sorted(keys):
         if dict_key not in dictionary_1:
-            children = create_difference_segment(VALUE_ADDED,
-                                                 dict_key,
-                                                 dictionary_2[dict_key])
+            children = {'type': VALUE_ADDED, 'key': dict_key,
+                        'value1': dictionary_2[dict_key], 'value2': None,
+                        'nested': None
+                        }
+
         elif dict_key not in dictionary_2:
-            children = create_difference_segment(VALUE_DELETED,
-                                                 dict_key,
-                                                 dictionary_1[dict_key])
+            children = {'type': VALUE_DELETED, 'key': dict_key,
+                        'value1': dictionary_1[dict_key], 'value2': None,
+                        'nested': None
+                        }
+
         elif dictionary_1[dict_key] == dictionary_2[dict_key]:
-            children = create_difference_segment(VALUE_UNCHANGED,
-                                                 dict_key,
-                                                 dictionary_1[dict_key])
+            children = {'type': VALUE_UNCHANGED, 'key': dict_key,
+                        'value1': dictionary_1[dict_key], 'value2': None,
+                        'nested': None
+                        }
+
         elif isinstance(dictionary_1[dict_key], dict) and isinstance(
                 dictionary_2[dict_key], dict):
-            children = create_difference_segment(
-                VALUE_NESTED,
-                dict_key,
-                value1=None,
-                nested=create_tree_vertex(dictionary_1[dict_key],
-                                          dictionary_2[dict_key]
-                                          )
-            )
+            children = {'type': VALUE_NESTED, 'key': dict_key,
+                        'value1': None, 'value2': None,
+                        'nested': create_tree_vertex(dictionary_1[dict_key],
+                                                     dictionary_2[dict_key]
+                                                     )}
         else:
-            children = create_difference_segment(VALUE_CHANGED,
-                                                 dict_key,
-                                                 value1=dictionary_1[
-                                                     dict_key],
-                                                 value2=dictionary_2[
-                                                     dict_key])
+            children = {'type': VALUE_CHANGED, 'key': dict_key,
+                        'value1': dictionary_1[dict_key],
+                        'value2': dictionary_2[dict_key], 'nested': None
+                        }
+
         list_result.append(children)
 
     return list_result
